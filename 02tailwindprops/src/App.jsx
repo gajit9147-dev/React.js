38fdc7fe50039157e6626e7a03b5a06c321590e6
Ajeet Gupta <229371738+gajit9147-dev@users.noreply.github.com>
2026-02-06 10:27:23 +0530

    react.js

---
+++ 02tailwindprops/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Card from './componant/card.jsx'
+
+function App() {
+  
+
+return (
+  <>
+    <Card userName="Shiv bhukta" company="Some Company" age={30}/>
+    <Card/>
+    <Card/>
+  </>
+)
+}
+
+export default App
